e2dee03c17feace82c95d5f183a2c5ec1278036e
nocaed <codenamedeacon@outlook.com>
2020-02-05 20:11:43 -0500

    Added javadoc comments

--- src/main/Team.java
+++ src/main/Team.java
@@ -10,13 +10,22 @@ public class Team
    private final int GROW_SIZE = 4; //initial and grow size
    private TeamMember [] team;
    private int numMembers;
-   
+
+   /**
+    * Default constructor for Team. Starts the array with a size of 4; the growth size, and starts the number of
+    * members at 0.
+    */
    public Team()
    {
       team = new TeamMember[GROW_SIZE];
       numMembers = 0;
    }
-   
+
+   /**
+    * Finds a specific TeamMember in team.
+    * @param m The target TeamMember that is being searched for in the array.
+    * @return The index of the target TeamMember. Returns -1 if m is not found.
+    */
    private int find(TeamMember m)
    {
        for(int i = 0; i < team.length; i++) {
@@ -32,7 +41,10 @@ public class Team
 
        return NOT_FOUND;
    }
-   
+
+   /**
+    * Grows the array by GROW_SIZE.
+    */
    private void grow()
    {
       TeamMember[] temp = new TeamMember[team.length + GROW_SIZE];
@@ -41,12 +53,20 @@ public class Team
       }
       team = temp;
    }
-   
+
+   /**
+    * Checks if the array is empty
+    * @return The evaluation of numMembers being equal to 0.
+    */
    public boolean isEmpty()
    {
        return numMembers == 0;
    }
-   
+
+   /**
+    * Adds a TeamMember to the team array.
+    * @param m The TeamMember to add to team.
+    */
    public void add(TeamMember m)
    {
       // TODO make storage more efficient by filling in null gaps created by object removal
@@ -55,7 +75,12 @@ public class Team
       team[numMembers] = m;
       numMembers++;
    }
-   
+
+   /**
+    * Remove a target TeamMember from the array.
+    * @param m The TeamMember to remove.
+    * @return True if the TeamMember was in the array, false if it wasn't.
+    */
    public boolean remove(TeamMember m)
    {
       int removalIndex = find(m);
@@ -63,9 +88,15 @@ public class Team
          return false;
       }
       team[removalIndex] = null;
+      numMembers--;
       return true;
-   } 
-   
+   }
+
+   /**
+    * Checks if team contains a target TeamMember.
+    * @param m The TeamMember being searched for.
+    * @return If team contains m or not.
+    */
    public boolean contains(TeamMember m)
    {
       for(TeamMember member : team) {
@@ -78,8 +109,11 @@ public class Team
          }
       }
       return false;
-   } 
-   
+   }
+
+   /**
+    * Prints each TeamMember in team.
+    */
    public void print()
    {
       for(TeamMember member : team) {
